d9cdd9434136242e046b9c4f57adb3fb86cc5c18
Louis Lacaille <llacaille@hellojoko.com>
2023-07-02 19:13:17 +0200

    FIX

--- main.py
+++ main.py
@@ -16,9 +16,10 @@ def traitement(image, prompt):
             "daanelson/minigpt-4:b96a2f33cc8e4b0aa23eacfce731b9c41a7d9466d9ed4e167375587b54db9423",
             input={"image": image_bytes, "prompt":prompt},
         )
+        return output
     except Exception as error:
         print(error)
-    return output
+        return(error)
 
 st.title("🩻 Delos Image Describer")
 
